0e3ee76ce42093c11aff0e7a57741f476f913030
superiorshrimp <szymon.zychowicz300701@gmail.com>
2023-01-18 17:26:37 +0100

    scala hello world reuslt

---
+++ main/hello_example.scala
@@ -0,0 +1,12 @@
+object HelloWorld{
+def main(args: Array[String]): Unit = {
+var i: Int = 10
+var j: Int = 3
+var f: Double = 0.0
+i=i+j-1*2/2
+var s: String = "Hello, World!"
+println(i*10)
+println("HEJO")
+
+}
+}
